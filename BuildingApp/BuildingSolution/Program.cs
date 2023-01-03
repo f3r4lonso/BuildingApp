0395db040927cc47f60c4891c8e6f98e2cc80509
f3r4lonso <alonso.fernando.morry@gmail.com>
2023-01-03 11:10:26 +0100

    added some unit test

---
+++ BuildingApp/BuildingSolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using BuildingSolution.Service;
+
+internal class Program
+{
+
+    static readonly IBildingService bildingService = new BildingService();
+
+    static readonly IWorkerDataLoader workerDataLoader = new WorkerDataLoader();
+
+    static List<Worker> workers;
+
+    static Building building;
+
+    private static void Main(string[] args)
+    {
+        workers = workerDataLoader.GetWorkers();
+
+        building = bildingService.CreateBuilding(workers);
+
+        int threadCount = bildingService.SetUpDailyWork(building);
+    }
+}
---
+++ BuildingApp/BuildingSolution/Service/IBildingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingSolution.Service
+{
+    internal interface IBildingService
+    {
+        int SetUpDailyWork(Building building);
+        Building CreateBuilding(List<Worker> Workers);
+    }
+}
